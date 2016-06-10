6ddb4514d807b4cb8258e0ca11822250492f6d31
Franck Silvestre <franck.silvestre@ticetime.com>
2016-06-10 10:58:35 +0200

    Update licence and copyright declaration

--- src/main/java/org/tsaap/lti/tp/Callback.java
+++ src/main/java/org/tsaap/lti/tp/Callback.java
@@ -1,28 +1,22 @@
 /*
-    LTIToolProvider - Classes to handle connections with an LTI 1 compliant tool consumer
-    Copyright (C) 2013  Stephen P Vickers
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation; either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-    Contact: stephen@spvsoftwareproducts.com
-
-    Version history:
-      1.0.00   2-Jan-13  Initial release
-      1.1.00  13-Apr-13
-      1.1.01  18-Jun-13
-*/
+ * LTIToolProvider - Classes to handle connections with an LTI 1 compliant tool consumer
+ *     Copyright (C) 2013  Stephen P Vickers
+ *
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation; either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License along
+ *     with this program; if not, see <http://www.gnu.org/licences/>
+ *
+ *     Contact: stephen@spvsoftwareproducts.com
+ */
 package org.tsaap.lti.tp;
 
 
